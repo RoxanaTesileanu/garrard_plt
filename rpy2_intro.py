916d1a4cab6206efaa386c58ea338216b9eed599
Roxana <roxana.te@web.de>
2016-10-19 16:30:29 +0300

    [first lm with rpy2]

--- rpy2_intro.py
+++ rpy2_intro.py
@@ -1,288 +1,6 @@
 Python 2.7.12 (default, Jul  1 2016, 15:12:24) 
 [GCC 5.4.0 20160609] on linux2
 Type "copyright", "credits" or "license()" for more information.
->>> # zonal analyses work on cells that share the same value, or belong to the same zone.
->>> import nunpy as np
-
-Traceback (most recent call last):
-  File "<pyshell#1>", line 1, in <module>
-    import nunpy as np
-ImportError: No module named nunpy
->>> import numpy as np
->>> import random
->>> arr= np.arange(0,21)
->>> arr
-array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
-       17, 18, 19, 20])
->>> arr2=np.random.sample(arr, 100)
-
-Traceback (most recent call last):
-  File "<pyshell#6>", line 1, in <module>
-    arr2=np.random.sample(arr, 100)
-  File "mtrand.pyx", line 1101, in mtrand.RandomState.random_sample (numpy/random/mtrand/mtrand.c:13875)
-TypeError: random_sample() takes at most 1 positional argument (2 given)
->>> arr2=np.random.sample(arr, 100, replace=True)
-
-Traceback (most recent call last):
-  File "<pyshell#7>", line 1, in <module>
-    arr2=np.random.sample(arr, 100, replace=True)
-  File "mtrand.pyx", line 1101, in mtrand.RandomState.random_sample (numpy/random/mtrand/mtrand.c:13875)
-TypeError: random_sample() takes at most 1 positional argument (2 given)
->>> arr2=np.random.choice(arr, 100)
->>> arr2
-array([15, 19, 19, 11, 19, 15, 17, 18,  2, 15,  2,  1,  2,  6, 14,  3,  6,
-        7, 10,  8, 20,  0,  4, 13, 11, 20, 17,  4, 17,  1, 16,  4,  1,  3,
-        1, 18, 19, 10,  2, 13,  7,  8, 10, 13, 20, 13,  4,  5, 12,  1, 11,
-        1,  7, 13,  4, 18, 15, 10,  4, 18, 15, 20, 15, 20,  0, 11,  4,  0,
-        8,  1, 10,  9,  6,  6, 10,  4,  6,  7, 11, 10, 20, 18, 13,  2,  6,
-        1, 11,  5, 13,  5,  7,  2,  4, 13, 13,  3, 19,  5,  3,  2])
->>> arr2.reshape(20,5)
-array([[15, 19, 19, 11, 19],
-       [15, 17, 18,  2, 15],
-       [ 2,  1,  2,  6, 14],
-       [ 3,  6,  7, 10,  8],
-       [20,  0,  4, 13, 11],
-       [20, 17,  4, 17,  1],
-       [16,  4,  1,  3,  1],
-       [18, 19, 10,  2, 13],
-       [ 7,  8, 10, 13, 20],
-       [13,  4,  5, 12,  1],
-       [11,  1,  7, 13,  4],
-       [18, 15, 10,  4, 18],
-       [15, 20, 15, 20,  0],
-       [11,  4,  0,  8,  1],
-       [10,  9,  6,  6, 10],
-       [ 4,  6,  7, 11, 10],
-       [20, 18, 13,  2,  6],
-       [ 1, 11,  5, 13,  5],
-       [ 7,  2,  4, 13, 13],
-       [ 3, 19,  5,  3,  2]])
->>> arr3=arr2.reshape(20,5)
->>> arr3
-array([[15, 19, 19, 11, 19],
-       [15, 17, 18,  2, 15],
-       [ 2,  1,  2,  6, 14],
-       [ 3,  6,  7, 10,  8],
-       [20,  0,  4, 13, 11],
-       [20, 17,  4, 17,  1],
-       [16,  4,  1,  3,  1],
-       [18, 19, 10,  2, 13],
-       [ 7,  8, 10, 13, 20],
-       [13,  4,  5, 12,  1],
-       [11,  1,  7, 13,  4],
-       [18, 15, 10,  4, 18],
-       [15, 20, 15, 20,  0],
-       [11,  4,  0,  8,  1],
-       [10,  9,  6,  6, 10],
-       [ 4,  6,  7, 11, 10],
-       [20, 18, 13,  2,  6],
-       [ 1, 11,  5, 13,  5],
-       [ 7,  2,  4, 13, 13],
-       [ 3, 19,  5,  3,  2]])
->>> # define bins:
->>> def get_bins(data) :
-	""" Return bin edges for all unique values in data. """
-	bins=np.unique(data)
-	return np.append(bins[~np.isnan(bins)], max(bins) +1)
-
->>> # use the numpy histogram2d function to get the counts
->>> # I need 2 arrays to make a histo, so I have to create the second one. I will use colors.
->>> col_arr=np.zeros((20,5), int)
->>> col_arr
-array([[0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0]])
->>> col_arr[0:6,0:3]='Red'
-
-Traceback (most recent call last):
-  File "<pyshell#23>", line 1, in <module>
-    col_arr[0:6,0:3]='Red'
-ValueError: invalid literal for int() with base 10: 'Red'
->>> col_arr[0:6,0:3].dtype=string
-
-Traceback (most recent call last):
-  File "<pyshell#24>", line 1, in <module>
-    col_arr[0:6,0:3].dtype=string
-NameError: name 'string' is not defined
->>> col_arr[0:6,0:3]=dtype(string)
-
-Traceback (most recent call last):
-  File "<pyshell#25>", line 1, in <module>
-    col_arr[0:6,0:3]=dtype(string)
-NameError: name 'dtype' is not defined
->>> col_arr[0:6,0:3]=string('Red')
-
-Traceback (most recent call last):
-  File "<pyshell#26>", line 1, in <module>
-    col_arr[0:6,0:3]=string('Red')
-NameError: name 'string' is not defined
->>> col_arr=np.zeros((20,5), bool)
->>> col_arr
-array([[False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False]], dtype=bool)
->>> col_arr[0:6,0:3]=True
->>> col_arr
-array([[ True,  True,  True, False, False],
-       [ True,  True,  True, False, False],
-       [ True,  True,  True, False, False],
-       [ True,  True,  True, False, False],
-       [ True,  True,  True, False, False],
-       [ True,  True,  True, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False]], dtype=bool)
->>> # I can't use strings...so I use false and true
->>> col_arr[7:16, 2:]=True
->>> col_arr
-array([[ True,  True,  True, False, False],
-       [ True,  True,  True, False, False],
-       [ True,  True,  True, False, False],
-       [ True,  True,  True, False, False],
-       [ True,  True,  True, False, False],
-       [ True,  True,  True, False, False],
-       [False, False, False, False, False],
-       [False, False,  True,  True,  True],
-       [False, False,  True,  True,  True],
-       [False, False,  True,  True,  True],
-       [False, False,  True,  True,  True],
-       [False, False,  True,  True,  True],
-       [False, False,  True,  True,  True],
-       [False, False,  True,  True,  True],
-       [False, False,  True,  True,  True],
-       [False, False,  True,  True,  True],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False],
-       [False, False, False, False, False]], dtype=bool)
->>> # I have to flatten the 2d arrays:
->>> hist, col_bins, arr3_bins = np.histogram2d(col_bins.flatten(), arr3.flatten(), [get_bins(col_arr), get_bins(arr3)])
-
-Traceback (most recent call last):
-  File "<pyshell#35>", line 1, in <module>
-    hist, col_bins, arr3_bins = np.histogram2d(col_bins.flatten(), arr3.flatten(), [get_bins(col_arr), get_bins(arr3)])
-NameError: name 'col_bins' is not defined
->>> hist, col_arr_bins, arr3_bins = np.histogram2d(col_bins.flatten(), arr3.flatten(), [get_bins(col_arr), get_bins(arr3)])
-
-Traceback (most recent call last):
-  File "<pyshell#36>", line 1, in <module>
-    hist, col_arr_bins, arr3_bins = np.histogram2d(col_bins.flatten(), arr3.flatten(), [get_bins(col_arr), get_bins(arr3)])
-NameError: name 'col_bins' is not defined
->>> np.histogram2d(col_bins.flatten(), arr3.flatten(), [get_bins(col_arr), get_bins(arr3)])
-
-Traceback (most recent call last):
-  File "<pyshell#37>", line 1, in <module>
-    np.histogram2d(col_bins.flatten(), arr3.flatten(), [get_bins(col_arr), get_bins(arr3)])
-NameError: name 'col_bins' is not defined
->>> hist, col_bins, arr3_bins = np.histogram2d(col_arr.flatten(), arr3.flatten(), [get_bins(col_arr), get_bins(arr3)])
->>> hist
-array([[ 0.,  5.,  4.,  3.,  5.,  3.,  3.,  2.,  2.,  1.,  2.,  5.,  0.,
-         6.,  1.,  3.,  1.,  1.,  3.,  3.,  2.],
-       [ 3.,  3.,  3.,  1.,  4.,  1.,  3.,  3.,  1.,  0.,  5.,  1.,  1.,
-         3.,  0.,  3.,  0.,  2.,  2.,  2.,  4.]])
->>> # two lists: one for the false values and one for the true values
->>> # each list contains the counts for each value from 0 to 20
->>> col_bins
-array([ 0.,  1.,  2.])
->>> arr3_bins
-array([  0.,   1.,   2.,   3.,   4.,   5.,   6.,   7.,   8.,   9.,  10.,
-        11.,  12.,  13.,  14.,  15.,  16.,  17.,  18.,  19.,  20.,  21.])
->>> # the histogram rows correspond to the bins from the first array that was passed in (two rows:false and true), and the columns correspond to the second array (arr3).
->>> 
->>> #########
->>> # using SciPy, you can get a histogram with the function called stats.binned_statistic_2d:
->>> import scipy.stats
->>> hist2, col_bins2, arr3_bins2, bn = scipy.stats.binned_statistic_2d(
-	col_arr.flatten(), arr3.flatten(),col_arr.flatten(), 'count', [get_bins(col_arr), get_bins(arr3)])
->>> hits2
-
-Traceback (most recent call last):
-  File "<pyshell#51>", line 1, in <module>
-    hits2
-NameError: name 'hits2' is not defined
->>> hist2
-array([[ 0.,  5.,  4.,  3.,  5.,  3.,  3.,  2.,  2.,  1.,  2.,  5.,  0.,
-         6.,  1.,  3.,  1.,  1.,  3.,  3.,  2.],
-       [ 3.,  3.,  3.,  1.,  4.,  1.,  3.,  3.,  1.,  0.,  5.,  1.,  1.,
-         3.,  0.,  3.,  0.,  2.,  2.,  2.,  4.]])
->>> bn
-array([62, 66, 66, 35, 43, 62, 64, 65, 26, 39, 49, 48, 49, 30, 38, 50, 53,
-       54, 34, 32, 67, 47, 51, 37, 35, 67, 64, 51, 41, 25, 40, 28, 25, 27,
-       25, 42, 43, 57, 49, 60, 31, 32, 57, 60, 67, 37, 28, 52, 59, 48, 35,
-       25, 54, 60, 51, 42, 39, 57, 51, 65, 39, 44, 62, 67, 47, 35, 28, 47,
-       55, 48, 34, 33, 53, 53, 57, 28, 30, 54, 58, 57, 44, 42, 37, 26, 30,
-       25, 35, 29, 37, 29, 31, 26, 28, 37, 37, 27, 43, 29, 27, 26])
->>> # I don't know what bn is...
->>> # ok, bn is binnumber - the bin in which an observation falls.
->>> import rpy
-
-Traceback (most recent call last):
-  File "<pyshell#56>", line 1, in <module>
-    import rpy
-ImportError: No module named rpy
->>> import rpy
-
-Traceback (most recent call last):
-  File "<pyshell#57>", line 1, in <module>
-    import rpy
-ImportError: No module named rpy
->>> import rpy
-
-Traceback (most recent call last):
-  File "<pyshell#58>", line 1, in <module>
-    import rpy
-ImportError: No module named rpy
->>> import rpy2
-
-Traceback (most recent call last):
-  File "<pyshell#59>", line 1, in <module>
-    import rpy2
-ImportError: No module named rpy2
 >>> import rpy2
 >>> print (r-y2._version_)
 
@@ -880,4 +598,135 @@ ImportError: No module named pandas
 >>> import pandas as pd
 >>> from rpy2.robjects import pandas2ri
 >>> pandas2ri.activate()
+>>> R = robjects.r
+>>> df= pd.DataFrame({'x':x, 'y':y})
+>>> df
+     x   y
+0    1   3
+1    2   4
+2    3   5
+3    4   6
+4    5   7
+5    6   8
+6    7   9
+7    8  10
+8    9  11
+9   10  12
+10  11  13
+11  12  14
+12  13  15
+13  14  16
+14  15  17
+15  16  18
+16  17  19
+17  18  20
+18  19  21
+19  20  22
+20  21  23
+21  22  24
+22  23  25
+23  24  26
+24  25  27
+25  26  28
+26  27  29
+27  28  30
+28  29  31
+>>> M=R.lm('y~x', data=df)
+>>> print(R.summary(M))
+
+Call:
+(function (formula, data, subset, weights, na.action, method = "qr", 
+    model = TRUE, x = FALSE, y = FALSE, qr = TRUE, singular.ok = TRUE, 
+    contrasts = NULL, offset, ...) 
+{
+    ret.x <- x
+    ret.y <- y
+    cl <- match.call()
+    mf <- match.call(expand.dots = FALSE)
+    m <- match(c("formula", "data", "subset", "weights", "na.action", 
+        "offset"), names(mf), 0L)
+    mf <- mf[c(1L, m)]
+    mf$drop.unused.levels <- TRUE
+    mf[[1L]] <- quote(stats::model.frame)
+    mf <- eval(mf, parent.frame())
+    if (method == "model.frame") 
+        return(mf)
+    else if (method != "qr") 
+        warning(gettextf("method = '%s' is not supported. Using 'qr'", 
+            method), domain = NA)
+    mt <- attr(mf, "terms")
+    y <- model.response(mf, "numeric")
+    w <- as.vector(model.weights(mf))
+    if (!is.null(w) && !is.numeric(w)) 
+        stop("'weights' must be a numeric vector")
+    offset <- as.vector(model.offset(mf))
+    if (!is.null(offset)) {
+        if (length(offset) != NROW(y)) 
+            stop(gettextf("number of offsets is %d, should equal %d (number of observations)", 
+                length(offset), NROW(y)), domain = NA)
+    }
+    if (is.empty.model(mt)) {
+        x <- NULL
+        z <- list(coefficients = if (is.matrix(y)) matrix(, 0, 
+            3) else numeric(), residuals = y, fitted.values = 0 * 
+            y, weights = w, rank = 0L, df.residual = if (!is.null(w)) sum(w != 
+            0) else if (is.matrix(y)) nrow(y) else length(y))
+        if (!is.null(offset)) {
+            z$fitted.values <- offset
+            z$residuals <- y - offset
+        }
+    }
+    else {
+        x <- model.matrix(mt, mf, contrasts)
+        z <- if (is.null(w)) 
+            lm.fit(x, y, offset = offset, singular.ok = singular.ok, 
+                ...)
+        else lm.wfit(x, y, w, offset = offset, singular.ok = singular.ok, 
+            ...)
+    }
+    class(z) <- c(if (is.matrix(y)) "mlm", "lm")
+    z$na.action <- attr(mf, "na.action")
+    z$offset <- offset
+    z$contrasts <- attr(x, "contrasts")
+    z$xlevels <- .getXlevels(mt, mf)
+    z$call <- cl
+    z$terms <- mt
+    if (model) 
+        z$model <- mf
+    if (ret.x) 
+        z$x <- x
+    if (ret.y) 
+        z$y <- y
+    if (!qr) 
+        z$qr <- NULL
+    z
+})(formula = "y~x", data = structure(list(x = 1:29, y = 3:31), .Names = c("x", 
+"y"), row.names = c("0", "1", "2", "3", "4", "5", "6", "7", "8", 
+"9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", 
+"20", "21", "22", "23", "24", "25", "26", "27", "28"), class = "data.frame"))
+
+Residuals:
+       Min         1Q     Median         3Q        Max 
+-1.098e-14 -3.518e-16 -3.400e-17  2.167e-16  1.114e-14 
+
+Coefficients:
+             Estimate Std. Error   t value Pr(>|t|)    
+(Intercept) 2.000e+00  1.171e-15 1.709e+15   <2e-16 ***
+x           1.000e+00  6.815e-17 1.467e+16   <2e-16 ***
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+Residual standard error: 3.071e-15 on 27 degrees of freedom
+Multiple R-squared:      1,	Adjusted R-squared:      1 
+F-statistic: 2.153e+32 on 1 and 27 DF,  p-value: < 2.2e-16
+
+
+>>> print(R.summary(M).rx2('coefficients'))
+            Estimate   Std. Error      t value Pr(>|t|)
+(Intercept)        2 1.170510e-15 1.708657e+15        0
+x                  1 6.814971e-17 1.467358e+16        0
+
 >>> 
+>>> 
+>>> 
+
