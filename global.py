2489296f6f5d335460c205cddb2fa94eb1aa84ec
Roxana <roxana.te@web.de>
2016-10-20 10:22:46 +0300

    [got the data files, started the proximity analysis]

--- global.py
+++ global.py
@@ -6,3 +6,26 @@ Type "copyright", "credits" or "license()" for more information.
 >>> # cost distance: determines the least cost of travelling between each cell and the nearest source, as determined by a cost surface
 >>> #(the easiest, and therefore least costly path may not be the shortest)
 >>> 
+>>> # 1) Proximity analysis : determine the distance to the nearest road
+>>> 
+>>> folder = '//home//roxana//private//osgeopy//osgeopy-data'
+>>> folder = '//home//roxana//private//osgeopy//osgeopy-data/osgeopy-data/Idaho'
+>>> roads_ln= 'wilderness'
+>>> roads_ln= 'allroads'
+>>> wilderness_ln='wilderness'
+>>> road_raster_fn='church_roads.tif'
+>>> proximity_fn='proximity.tif'
+>>> cellsize=10
+>>> 
+>>> shp_ds=ogr.Open(folder)
+
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    shp_ds=ogr.Open(folder)
+NameError: name 'ogr' is not defined
+>>> import os
+>>> import sys
+>>> from osgeo import gdal, ogr
+>>> shp_ds=ogr.Open(folder)
+>>> wild_lyr = shp_ds.GetLayerByName(wilderness_ln)
+>>> 
