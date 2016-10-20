5c85ab37b1b527e418c339b981a845bd71444ca5
Roxana <roxana.te@web.de>
2016-10-20 09:47:11 +0300

    [start global analyses]

---
+++ global.py
@@ -0,0 +1,8 @@
+Python 2.7.12 (default, Jul  1 2016, 15:12:24) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> # global analyses
+>>> # e.g. proximity analysis: determines the Euclidean distance of each cell to the nearest cell that's marked as a source
+>>> # cost distance: determines the least cost of travelling between each cell and the nearest source, as determined by a cost surface
+>>> #(the easiest, and therefore least costly path may not be the shortest)
+>>> 
